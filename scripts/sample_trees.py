64dfedf18291007cb82411bb884628e35afa9ad7
Mark Madsen <mark@madsenlab.org>
2020-07-06 16:50:03 -0700

    parallelized with Ray to do 1B graphs

--- scripts/sample_trees.py
+++ scripts/sample_trees.py
@@ -7,26 +7,26 @@ import matplotlib.pyplot as plt
 
 @ray.remote
 def processor_task(work_item):
-    eigen_list = []
-    for _ in range(10):
-        g = nx.generators.trees.random_tree(20)
-        evals = nx.laplacian_spectrum(g)
-        eigen_list.append(list(evals))
-    return eigen_list
+	g = nx.generators.trees.random_tree(20)
+	evals = nx.laplacian_spectrum(g).tolist()
+	return evals 
 
 
 
 if __name__ == "__main__":
-    ray.init(webui_host='0.0.0.0', address="127.0.0.1:9999")
+	ray.init(webui_host='0.0.0.0')
 
-    with open("random_tree_spectra.csv", 'w') as f:
-        futures = [processor_task.remote(s) for _ in range(10)]
-        print(futures)
-        while len(futures) > 0:
-            finished, rest = ray.wait(futures, timeout=1)
-            for id in futures:
-                res_list = ray.get(id)
-                for row in res_list:
-                    f.write(",".join(row))
-            print(f"Incomplete: {len(rest)}")
-            futures = rest
+
+	for i in range(1000):
+		with open(f"/data/experiments/tree-spectra/random_tree_spectra_{i}.csv", 'w') as f:
+			print(f"Starting batch {i} of 10MM graphs...")
+			for i in range(10000):
+				futures = [processor_task.remote(s) for s in range(100)]
+				while len(futures) > 0:
+					finished, rest = ray.wait(futures, timeout=1)
+
+					for id in finished:
+						res_list = ray.get(id)
+						f.write(",".join(map(str, res_list)))
+						f.write("\n")
+					futures = rest
